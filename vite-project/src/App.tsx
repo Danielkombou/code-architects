4ba95af99d7b775450059ebbb3f63ba972080a0c
Danielkombou <danielkombou7@gmail.com>
2025-01-30 11:05:42 +0100

    animations with framer; grid to list display

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -23,28 +23,14 @@ const Grid = () => {
       <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
         {Array.from({ length: 10 }).map((_, index) => {
           return (
-            <motion.div layoutId={`grid-items-${index}`} className=" bg-slate-300 rounded-md p-4" key={index}>
-              <div className="flex justify-between items-center mb-4">
-                {/* user profile */}
-                <div className="flex items-center gap-2">
-                  <motion.div layoutId="avatar" className="rounded-full bg-slate-600 p-5" />
-                  <div className="">
-                    <motion.p layoutId="name" className="font-semibold">John Doe</motion.p>
-                    <motion.span layoutId="email"
-                      className="text-slate-600">john@doe.com</motion.span>
-                  </div>
+            <motion.div layoutId={`items-${index}`} className="flex gap-4 flex-col bg-slate-300 rounded-md p-4" key={index}>
+              <div className="flex items-center justify-between">
+                <ProfileSection index={index} />
+                <div className="cursor-pointer rounded-full bg-slate-300 border-slate-600 border-2 p-2">
+                  <Trash2 className="w-5 h-5 " />
                 </div>
-
-                {/* Trash */}
-                <motion.div layoutId="delete" className="flex items-center border-2 cursor-pointer border-slate-600 p-2 rounded-full">
-                  <Trash2 className="w-4 h-4" />
-                </motion.div>
               </div>
-              {/* Bars */}
-              <motion.div layoutId="bar-component" className="flex flex-col gap-2">
-                <motion.div layoutId="bar-one" className="bg-slate-600 p-1 w-full h-2 rounded-md" />
-                <motion.div layoutId="bar-two" className="bg-slate-600 p-1 w-1/2 h-2 rounded-md" />
-              </motion.div>
+              <BarsSection index={index} />
             </motion.div>
           )
         })}
@@ -58,25 +44,11 @@ const List = () => {
     <motion.div layout className="grid grid-cols-1 gap-5">
       {Array.from({ length: 10 }).map((_, index) => {
         return (
-          <motion.div layoutId={`list-items-${index}`} className=" bg-slate-300 rounded-md p-4" key={index}>
-            <div className="flex justify-between items-center gap-4">
-              {/* user profile */}
-              <div className="flex items-center gap-2">
-                <motion.div layoutId="list-profile" className="rounded-full bg-slate-600 p-5" />
-                <div className="">
-                  <motion.p layoutId="list-name" className="font-semibold">John Doe</motion.p>
-                  <motion.span layoutId="list-email" className="text-slate-600">john@doe.com</motion.span>
-                </div>
-              </div>
-              {/* bars */}
-              <motion.div layoutId="list-bar" className="flex flex-col w-full gap-2">
-                <motion.div layoutId="list-bar-1" className="bg-slate-600 p-1 w-full rounded-md" />
-                <motion.div layoutId="list-bar-1" className="bg-slate-600 p-1 w-1/2 rounded-md" />
-              </motion.div>
-              {/* trash */}
-              <div className="flex items-center border-2 cursor-pointer border-slate-600 p-2 rounded-full">
-                <Trash2 className="w-4 h-4" />
-              </div>
+          <motion.div layoutId={`items-${index}`} className="flex items-center gap-4 bg-slate-300 rounded-md p-4" key={index}>
+            <ProfileSection index={index} />
+            <BarsSection index={index} className="w-full" />
+            <div className="rounded-full bg-slate-300 border-slate-600 border-2 p-2">
+              <Trash2 className="w-5 h-5 " />
             </div>
           </motion.div>
         )
@@ -84,6 +56,40 @@ const List = () => {
     </motion.div>
   )
 }
+
+
+const ProfileSection = ({ index }: { index: number }) => (
+  <div className="flex items-center gap-2">
+    {/* <div className="flex items-center gap-2"> */}
+    <motion.div
+      layoutId={`avatar-${index}`}
+      className="rounded-full bg-slate-600 p-5"
+    />
+    <div>
+      <motion.p layoutId={`name-${index}`} className="font-semibold">
+        John Doe
+      </motion.p>
+      <motion.span layoutId={`email-${index}`} className="text-slate-600">
+        john@doe.com
+      </motion.span>
+    </div>
+    {/* </div> */}
+
+  </div>
+);
+
+const BarsSection = ({ index, className = "" }: { index: number; className?: string }) => (
+  <motion.div layout className={`flex flex-col gap-2 ${className}`}>
+    <motion.div
+      layoutId={`bar-${index}`}
+      className="bg-slate-600 p-1 w-full rounded-md"
+    />
+    <motion.div
+      layoutId={`bar-small-${index}`}
+      className="bg-slate-600 p-1 w-1/2 rounded-md"
+    />
+  </motion.div>
+);
 
 type HeaderProps = {
   view: ViewType;
@@ -96,24 +102,27 @@ const Header = ({ view, onViewChange }: HeaderProps) => {
       <h1 className="text-2xl font-semibold">Grid to List</h1>
       <motion.div
         layout
-        className="flex items-center gap-2 bg-slate-300 p-1 rounded-md ">
+        className="relative flex items-center gap-2 bg-slate-300 p-1 rounded-md ">
         {listItem.map((item) => (
           <button
             key={item.name}
-            className={`cursor-pointer p-2 rounded-md ${view === item.name ? "bg-slate-600" : " "
+            className={`relative z-10 transition-all duration-300 cursor-pointer px-3 py-2.5 rounded-md ${view === item.name ? "text-white" : "text-black"
               }`}
             onClick={() => onViewChange(item.name)}
           >
             <item.icon className={`w-5 h-5 ${view !== item.name ? "text-black" : ""} text-white/90`} />
           </button>
         ))}
-         <motion.div
+        <motion.div
           layoutId="grid-line"
-          className="size-10 bg-background absolute rounded-md"
+          className="absolute top-1 left-1 size-10 bg-slate-600 rounded-md"
           animate={{
             x: view === "grid" ? 0 : 56,
             transition: {
-              duration: 1.5,
+              damping: 30,
+              stiffness: 150,
+              type: "spring",
+              duration: 0.2,
             },
           }}
         />
@@ -133,29 +142,29 @@ function App() {
     <>
       <div className='mx-auto max-w-5xl bg-slate-100 border-x p-20 w-full min-h-screen'>
         <Header view={view} onViewChange={handleViewChange} />
-        <AnimatePresence initial={false} mode="popLayout">
+        <AnimatePresence initial={false} mode="wait">
           {
             view === "grid" ? (
               <motion.div
-              key="grid"
-              initial={{ opacity: 0, y: 20, x: 0 }}
-              animate={{ opacity: 1, y: 0, x: 20 }}
-              exit={{ opacity: 0, x: 0, y: 20 }}
-              layoutId="gridView"
-              transition={{
-                
-                duration: 2,
-              }}
+                key="grid"
+                // initial={{ opacity: 0, y: 0, x: 0 }}
+                // animate={{ opacity: 1, y: 10, x: 0 }}
+                // exit={{ opacity: 0, x: 0, y: 0, }}
+                layoutId="gridView"
+                transition={{
+
+                  duration: 0.2,
+                }}
               >
                 <Grid />
               </motion.div>) : (
               <motion.div
-              key="list"
-              initial={{ opacity: 0, y: 20, x: 0 }}
-              animate={{ opacity: 1, y: 0, x: 20 }}
-              exit={{ opacity: 0, y: 20, x: 0 }}
-              layoutId="listView"
-              transition={{ type: "spring", duration: 2 }}
+                key="list"
+                // initial={{ opacity: 0, y: 40, x: 0 }}
+                // animate={{ opacity: 1, y: 0, x: 20 }}
+                // exit={{ opacity: 0, y: 40, x: 0 }}
+                layoutId="gridView"
+                transition={{ type: "spring", duration: 0.2 }}
               >
                 <List />
               </motion.div>)}
